75430b28fd0e8d6649ce0a437204513e0e3d46d8
Tu Nombre <tu@email.com>
2026-03-01 03:18:54 +0100

    Ajuste de urls y fotos

--- config/urls.py
+++ config/urls.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from menu.views import lista_productos
-from django.conf import settings # Agrega esto
-from django.conf.urls.static import static # Agrega esto
+from django.conf import settings 
+from django.conf.urls.static import static 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', lista_productos, name='home'),
 ]
 
-# Esto permite que Django muestre las fotos que subiste
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Esto asegura que las fotos se vean en Render
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
